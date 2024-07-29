56c019bc44320695552b42b89be0ae096546b190
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2024-07-29 20:11:55 -0300

    api base
    Baixei o modelo da api spring boot

---
+++ Flyticket/src/test/java/br/com/Api/Flyticket/FlyticketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Api.Flyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
